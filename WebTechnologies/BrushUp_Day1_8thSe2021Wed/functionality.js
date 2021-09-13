890737400dc077976634297bda509fe9c362c64b
Balaji Ganesh <42509652+Balaji-Ganesh@users.noreply.github.com>
2021-09-13 07:16:17 +0530

    Initial Commit

---
+++ WebTechnologies/BrushUp_Day1_8thSe2021Wed/functionality.js
@@ -0,0 +1,18 @@
+$(document).ready()
+{
+    $('button').click(function(){
+        alert("test");
+        /*
+        // Get the data in the text box..
+        const textBoxValue = $('#inputBox').val();
+        alert(textBoxValue);
+
+        // Change the image accordingly..
+        $('.imageDisplay').attr('src', 'https:////joeschmoe.io/api/v1/{textBoxValue}');*/
+    })
+
+    function changeImage()
+    {
+       alert("Hello dx..!!");
+    }
+}
